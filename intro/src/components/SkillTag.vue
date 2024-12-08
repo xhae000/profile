062831bc3d3da6dba417c573370c4ead19d43982
woooo <99981511+xhae000@users.noreply.github.com>
2024-12-09 01:33:48 +0900

    init

---
+++ intro/src/components/SkillTag.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="inline-block px-3 py-[0.2rem] text-[0.75rem] rounded-2xl mr-1"
+        :style="{
+            backgroundColor : boxColor,
+            color : textColor,
+        }">
+        {{ name }}
+    </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+// props 정의
+defineProps({
+    name: {
+        type: String,
+        required: true,
+    },
+    boxColor: {
+        type: String,
+        required: true,
+    },
+    textColor: {
+        type: String,
+        required: true,
+    },
+})
+</script>
